73e9c7b8e6d2e37e97f2e35950afc02d7c347a5c
Lukas Dürrenberger <eXpl0it3r@sfml-dev.org>
2019-06-01 21:00:28 +0200

    Initial commit with setup project

---
+++ 01-Setup/src/main.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	auto window = sf::RenderWindow{ {1280u, 720u}, "01 Setup - SFML Workshop" };
+	window.setFramerateLimit(60);
+
+	auto rectangle = sf::RectangleShape{ {100.f, 100.f} };
+	rectangle.setFillColor(sf::Color::Green);
+	rectangle.setPosition({ 50.f, 50.f });
+
+	while(window.isOpen())
+	{
+		for(auto event = sf::Event{}; window.pollEvent(event);)
+		{
+			if(event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(rectangle);
+		window.display();
+	}
+}
